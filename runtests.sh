84ac1b52c521eafee0c6a247d2a70fef84f095d4
jbelyeu <jrbelyeu@gmail.com>
2020-04-29 18:38:24 -0600

    cleaned up test runner

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,3 @@
-#echo "running unit tests:"
-#python test/unit/unfazed_test.py
-#echo "finished unit tests"
-
 echo "running functional tests:"
 bash test/func/unfazed_test.sh
 echo "finished functional tests"
